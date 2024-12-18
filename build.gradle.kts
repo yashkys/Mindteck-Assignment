08fff17d04bdb54f8ed144bc6c078a378f9d3851
Yash Khaitan <yashkhaitan02@gmail.com>
2024-12-18 09:54:23 +0530

    Project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
 }
